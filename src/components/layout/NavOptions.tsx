4673e90be57d2a6c2a7c2c07bb532085648480ab
SanCT5 <sanct1318@gmail.com>
2024-09-10 15:52:35 -0500

    FEAT: add profile route on topbar

--- src/components/layout/NavOptions.tsx
+++ src/components/layout/NavOptions.tsx
@@ -1,4 +1,4 @@
-import { AccountBox, Dashboard, Announcement, Groups, Sell } from '@mui/icons-material';
+import { Dashboard, Announcement, Groups, Sell } from '@mui/icons-material';
 import { Role } from '../../hooks/users/userSlice';
 import React from 'react';
 
@@ -14,5 +14,4 @@ export const userOptions: NavOptions[] = [
     { text: 'Anuncios', icon: <Announcement />, link: '/app/announcements', role: ['ADMIN', 'RESIDENT'] },
     { text: 'Publicidad', icon: <Sell />, link: '/app/ads', role: ['ADMIN', 'RESIDENT'] },
     { text: 'Residentes', icon: <Groups />, link: '/app/residents', role: ['ADMIN'] },
-    { text: 'Perfil', icon: <AccountBox />, link: '/app/profile', role: ['ADMIN', 'RESIDENT'] },
 ];
