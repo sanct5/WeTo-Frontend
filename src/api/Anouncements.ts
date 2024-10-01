03b95c24306c57a6d3aa0271db5a59b0719e46fd
SanCT5 <sanct1318@gmail.com>
2024-10-01 16:50:52 -0500

    STYLE: update api routes

--- src/api/Anouncements.ts
+++ src/api/Anouncements.ts
@@ -1,12 +1,12 @@
 export const AnnouncementsService = {
     baseUrl: import.meta.env.VITE_WETO_BACKEND_ANNOUNCEMENTS,
     endpoints: {
-        GetAnnouncementsByComplex: '/getAnounsByComplex',
-        GetAnnouncementsByUser: '/getAnounsByUser',
-        GetAnnouncementById: '/getAnounById',
-        UpdateAnnouncement: '/updateAnoun',
-        AddAnnouncement: '/addAnoun',
-        DeleteAnnouncement: '/deleteAnoun',
-        SearchAnnouncements: '/searchAnnouncements',
+        GetAnnouncementsByComplex: '/getByComplex',
+        GetAnnouncementsByUser: '/getByUser',
+        GetAnnouncementById: '/getById',
+        UpdateAnnouncement: '/update',
+        AddAnnouncement: '/add',
+        DeleteAnnouncement: '/delete',
+        SearchAnnouncements: '/search',
     },
 };
--- src/api/ComplexService.ts
+++ src/api/ComplexService.ts
@@ -1,10 +1,10 @@
 export const ComplexService = {
     baseUrl: import.meta.env.VITE_WETO_BACKEND_COMPLEX,
     endpoints: {
-        ComplexColors: '/getComplexColors',
-        DeleteComplex: '/deleteComplex',
-        GetComplex: '/getComplex',
-        AddComplex: '/addComplex',
-        UpdateComplexColors: '/updateComplexColors',    
+        ComplexColors: '/getColors',
+        DeleteComplex: '/delete',
+        GetComplex: '/get',
+        AddComplex: '/add',
+        UpdateComplexColors: '/updateColors',    
     },
 };
