441e854062b3fa6ee3418fa5c787539cf4ed0c71
SanCT5 <sanct1318@gmail.com>
2024-10-20 01:33:56 -0500

    STYLE: change console.log  for console.info

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -76,9 +76,9 @@ const SignIn = () => {
             deferredPrompt.prompt();
             deferredPrompt.userChoice.then((choiceResult) => {
                 if (choiceResult.outcome === 'accepted') {
-                    console.log('User accepted the A2HS prompt');
+                    console.info('User accepted the PWA prompt');
                 } else {
-                    console.log('User dismissed the A2HS prompt');
+                    console.info('User dismissed the PWA prompt');
                 }
                 setDeferredPrompt(null);
             });
